fb362622e4e572f0978db3f13da0ecc740df2da5
Gbrlvcas <Gbrlvcas@DESKTOP-CJ930B3>
2018-05-29 00:12:26 -0300

    Criado o programa Priston Tale

---
+++ src/codigo/Acao.java
@@ -0,0 +1,5 @@
+package codigo;
+
+public class Acao {
+
+}
---
+++ src/codigo/PristonTale.java
@@ -0,0 +1,10 @@
+package codigo;
+
+public class PristonTale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
